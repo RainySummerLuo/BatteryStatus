44569e606770909ede2f1e92b8dc0d30ebd19305
Laurence Luo <iluo@163.com>
2020-06-27 14:42:35 +0800

    First draft

--- BatteryStatus/Form1.Designer.cs
+++ BatteryStatus/Form1.Designer.cs
@@ -23,13 +23,87 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.lblBatteryName = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatusDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBatteryName
+            // 
+            this.lblBatteryName.AutoSize = true;
+            this.lblBatteryName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBatteryName.Location = new System.Drawing.Point(22, 24);
+            this.lblBatteryName.Name = "lblBatteryName";
+            this.lblBatteryName.Size = new System.Drawing.Size(135, 28);
+            this.lblBatteryName.TabIndex = 0;
+            this.lblBatteryName.Text = "Battery Name:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(92, 65);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(65, 28);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(27, 185);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(597, 36);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 100;
+            // 
+            // lblStatusDescription
+            // 
+            this.lblStatusDescription.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusDescription.Location = new System.Drawing.Point(92, 103);
+            this.lblStatusDescription.Name = "lblStatusDescription";
+            this.lblStatusDescription.Size = new System.Drawing.Size(549, 136);
+            this.lblStatusDescription.TabIndex = 2;
+            this.lblStatusDescription.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(653, 257);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatusDescription);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblBatteryName);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblBatteryName;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatusDescription;
     }
 }
 
--- BatteryStatus/Form1.cs
+++ BatteryStatus/Form1.cs
@@ -1,16 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Management;
 using System.Windows.Forms;
 
 namespace BatteryStatus {
     public partial class Form1 : Form {
+        Dictionary<ushort, string> StatusCodes;
+        Dictionary<ushort, string> StatusDescription;
+        Dictionary<ushort, string> BatteryChemistry;
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            StatusCodes = new Dictionary<ushort, string> {
+                { 1, "Other (1)" },
+                { 2, "Unknown (2)" },
+                { 3, "Fully Charged (3)" },
+                { 4, "Low (4)" },
+                { 5, "Critical (5)" },
+                { 6, "Charging (6)" },
+                { 7, "Charging and High (7)" },
+                { 8, "Charging and Low (8)" },
+                { 9, "Charging and Critical (9)" },
+                { 10, "Undefined (10)" },
+                { 11, "Partially Charged (11)" }
+            };
+
+            StatusDescription = new Dictionary<ushort, string> {
+                { 1, "The battery is discharging." },
+                { 2, "The system has access to AC so no battery is being discharged. However, the battery is not necessarily charging." },
+            };
+
+            BatteryChemistry = new Dictionary<ushort, string> {
+                { 1, "Other (1)" },
+                { 2, "Unknown (2)" },
+                { 3, "Lead Acid (3)" },
+                { 4, "Nickel Cadmium (4)" },
+                { 5, "Nickel Metal Hydride (5)" },
+                { 6, "Lithium-ion (6)" },
+                { 7, "Zinc air (7)" },
+                { 8, "Lithium Polymer (8)" },
+            };
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
+
+            string caption = null, name = null, status = null, statusDescription = null, batteryChemistry = null;
+
+            foreach (ManagementObject mo in mos.Get()) {
+                caption = mo["Caption"].ToString();
+
+                name = mo["Name"].ToString();
+
+                ushort statuscode = (ushort)mo["BatteryStatus"];
+
+                ushort batterychemistrycode = (ushort)mo["Chemistry"];
+
+                status = StatusCodes[statuscode];
+
+                if (StatusDescription.ContainsKey(statuscode)) {
+                    statusDescription = StatusDescription[statuscode];
+                } else {
+                    statusDescription = "[None]";
+                }
+
+                batteryChemistry = BatteryChemistry[batterychemistrycode];
+            }
+
+            Text = "Battery Status - " + caption;
+            lblBatteryName.Text = "Battery Name:   " + name + " / Chemistry:   " + batteryChemistry;
+            lblStatus.Text = status;
+            lblStatusDescription.Text = statusDescription;
         }
     }
 }
